c3493a99770dcb3e06e5c4e1a09607d89807091c
DariaIS <dashastarodubovapenza@mail.ru>
2021-11-07 20:54:59 +0300

    first big commit with scss, and working header jsx component but without route stuff

---
+++ src/components/HeaderSecurity.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HeaderSecurity() {
+    return (
+        <div className="header">
+            <div className="container">
+            </div>
+        </div>
+    );
+}
+
+export default HeaderSecurity;
