cb705a17056ff78e338cd01fc414a2087c4d35b6
DariaIS <dariastarodubova58@yandex.ru>
2021-12-03 19:55:49 +0300

    did sign in check and error message

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,6 +6,11 @@ function SignIn() {
     const [usernameReg, setUsernameReg] = useState('');
     const [passwordReg, setPasswordReg] = useState('');
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [loginStatus, setLoginStatus] = useState('');
+
     const register = () => {
         Axios.post('http://localhost:3001/register', {
             username: usernameReg, 
@@ -15,29 +20,60 @@ function SignIn() {
         });
     };
 
+    const login = () => {
+        Axios.post('http://localhost:3001/login', {
+            username: username, 
+            password: password
+        }).then((response) => {
+            if (response.data.message)
+                setLoginStatus(response.data.message);
+            else setLoginStatus(response.data[0].username);
+        });
+    };
+
     return (
         <div className="signin container">
             <div className="signin__logo">
                 <span>Пропускная</span>
                 <span>Система</span>
             </div>
-            <form className="signin__form form">
-                <label className="form__item">
-                    <input className="input input--small input--default" placeholder="Имя пользователя" type="text" name="name" 
-                        onChange={(e) => {
-                            setUsernameReg(e.target.value);
-                        }}
-                    />
-                </label>
-                <label className="form__item">
-                    <input className="input input--small input--default" placeholder="Пароль" type="password" name="password"
-                        onChange={(e) => {
-                            setPasswordReg(e.target.value);
-                        }}
-                    />
-                </label>
-                <input className="input__submit" type="submit" value="Войти" onClick={register}/>
-            </form>
+            <div className="signin__forms">
+                <form className="signin__form form">
+                    <label className="form__item">
+                        <input className="input input--small input--default" placeholder="Имя пользователя" type="text" name="name" 
+                            onChange={(e) => {
+                                setUsernameReg(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <label className="form__item">
+                        <input className="input input--small input--default" placeholder="Пароль" type="password" name="password"
+                            onChange={(e) => {
+                                setPasswordReg(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <button type='button' className="button" onClick={register}>Зарегистрироваться</button>
+                </form>
+                <form className="signin__form form">
+                    <label className="form__item">
+                        <input className="input input--small input--default" placeholder="Имя пользователя" type="text" name="name"
+                            onChange={(e) => {
+                                setUsername(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <label className="form__item">
+                        <input className="input input--small input--default" placeholder="Пароль" type="password" name="password"
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <button type='button' className="button signin__button" onClick={login}>Войти</button>
+                    <span className="signin__status">{loginStatus}</span>
+                </form>
+            </div>
         </div>
     );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -28,14 +28,32 @@ db.connect(function (err) {
 });
 
 app.post("/register", (req, res) => {
-
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result) => {
-        console.log(err);
+    db.query(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, password], (err, result) => {
+            console.log(err);
     })
 });
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password], 
+        (err, result) => {
+            if (err)
+                res.send({err: err});
+
+            if (result.length > 0)
+                res.send(result);
+            else res.send({message: "Неверный логин или пароль!"})
+    })
+})
 
 app.listen(3001, () => {
     console.log("running server");
