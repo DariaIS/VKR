ac504f00f6a6d5fa5b61acf0edd773bba8ea3370
DariaIS <dariastarodubova58@yandex.ru>
2021-12-03 13:56:04 +0300

    add express

--- src/App.js
+++ src/App.js
@@ -6,28 +6,8 @@ import { HeaderSecurity } from './components';
 import { HomeSecurity, SignIn } from './pages';
 import { Route, Routes } from 'react-router-dom';
 
-
 import Footer from './components/Footer';
 import Button from './components/Button';
-
-const mysql = require('mysql');
-
-const conn = mysql.createConnection({
-  host: "127.0.0.1", 
-  port: '3306',
-  user: 'root',
-  database: "vkr",
-});
-
-conn.connect(function (err) {
-  if (err) {
-      return console.error("Ошибка: " + err.message);
-  }
-  else {
-      console.log("Подключение к серверу MySQL успешно установлено");
-  }
-});
-
 
 function App() {
   return (
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn() {
+
+    const [usernameReg, setUsernameReg] = useState('');
+    const [passwordReg, setPasswordReg] = useState('');
+
     return (
         <div className="signin container">
             <div className="signin__logo">
@@ -9,12 +13,20 @@ function SignIn() {
             </div>
             <form className="signin__form form">
                 <label className="form__item">
-                    <input className="input input--small input--default" placeholder="Имя пользователя" type="text" name="name" />
+                    <input className="input input--small input--default" placeholder="Имя пользователя" type="text" name="name" 
+                        onChange={(e) => {
+                            setUsernameReg(e.target.value);
+                        }}
+                    />
                 </label>
                 <label className="form__item">
-                    <input className="input input--small input--default" placeholder="Пароль" type="password" name="password" />
+                    <input className="input input--small input--default" placeholder="Пароль" type="password" name="password"
+                        onChange={(e) => {
+                            setPasswordReg(e.target.value);
+                        }}
+                    />
                 </label>
-                <input className="input__submit" type="submit" value="Отправить" />
+                <input className="input__submit" type="submit" value="Войти" />
             </form>
         </div>
     );
