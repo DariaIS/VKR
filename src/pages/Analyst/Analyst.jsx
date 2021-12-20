059e4743b387791921adf91b6c194c2791dfaa4a
DariaIS <dariastarodubova58@yandex.ru>
2021-12-20 13:43:02 +0300

    did analyst table

--- src/pages/Analyst/Analyst.jsx
+++ src/pages/Analyst/Analyst.jsx
@@ -3,8 +3,43 @@ import React from "react";
 function Analyst() {
 
     return (
-        <div>
-            <h1>Analyst</h1>
+        <div className="analyst section container">
+            <span className="analyst__title title title--medium">Вы вошли как аналитик</span>
+
+            <div className="analyst__table">
+                <span className="analyst__table-title title title--small">Все машины, присутствующие в базе данных</span>
+                <table className="analyst__table-item">
+                    <thead className="analyst__thead">
+                        <tr className="analyst__tr">
+                            <th className="analyst__th">Company</th>
+                            <th className="analyst__th">Contact</th>
+                            <th className="analyst__th">Country</th>
+                            <th className="analyst__th">Country</th>
+                        </tr>
+                    </thead>
+                    <tbody className="analyst__tbody">
+                        <tr className="analyst__tr">
+                            <td className="analyst__td">Alfreds Futterkiste</td>
+                            <td className="analyst__td">Maria Anders</td>
+                            <td className="analyst__td">Germany</td>
+                            <td className="analyst__td">Germany</td>
+
+                        </tr>
+                        <tr className="analyst__tr">
+                            <td className="analyst__td">Alfreds Futterkiste</td>
+                            <td className="analyst__td">Maria Anders</td>
+                            <td className="analyst__td">Germany</td>
+                            <td className="analyst__td">Germany</td>
+                        </tr>
+                        <tr className="analyst__tr">
+                            <td className="analyst__td">Alfreds Futterkiste</td>
+                            <td className="analyst__td">Maria Anders</td>
+                            <td className="analyst__td">Germany</td>
+                            <td className="analyst__td">Germany</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
