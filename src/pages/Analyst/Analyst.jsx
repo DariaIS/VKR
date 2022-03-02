674e08e1aeab5c869ded615052278e9dad7e73c9
DariaIS <dariastarodubova58@yandex.ru>
2022-03-02 18:17:14 +0300

    install react-export-excel

--- src/pages/Analyst/Analyst.jsx
+++ src/pages/Analyst/Analyst.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+
+import ReactExport from "react-export-excel";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+
 import Axios from 'axios';
 
 function Analyst() {
@@ -9,6 +12,10 @@ function Analyst() {
     const [dateTable, setDateTable] = useState('');
 
     const [pickedDate, setPickedDate] = useState(new Date());
+
+    const ExcelFile = ReactExport.ExcelFile;
+    const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+    const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
     const date = (newDate) => {
         setPickedDate(newDate);
@@ -40,32 +47,44 @@ function Analyst() {
                     <div>
                         <span className="analyst__table-title title title--small">Отчет о въездах и выездах на {pickedDate.toLocaleDateString()}</span>
                         <table className="analyst__table-item">
-                        <thead className="analyst__thead">
-                            <tr className="analyst__tr">
-                                <th className="analyst__th">Номер автомобиля</th>
-                                <th className="analyst__th">Регион</th>
-                                <th className="analyst__th">Марка автомобиля</th>
-                                <th className="analyst__th">Время въезда</th>
-                                <th className="analyst__th">Время выезда</th>
-                            </tr>
-                        </thead>
-                        <tbody className="analyst__tbody">
-                            {
-                                Object.values(dateTable).map(val => {
-                                    return (
-                                        <tr className="analyst__tr" key={val.id_car}>
-                                            <td className="analyst__td">{val.license_plate}</td>
-                                            <td className="analyst__td">{val.region}</td>
-                                            <td className="analyst__td">{val.car_brand}</td>
-                                            <td className="analyst__td">{val.arrival_time}</td>
-                                            <td className="analyst__td">{val.departure_time}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                            <thead className="analyst__thead">
+                                <tr className="analyst__tr">
+                                    <th className="analyst__th">Номер автомобиля</th>
+                                    <th className="analyst__th">Регион</th>
+                                    <th className="analyst__th">Марка автомобиля</th>
+                                    <th className="analyst__th">Время въезда</th>
+                                    <th className="analyst__th">Время выезда</th>
+                                </tr>
+                            </thead>
+                            <tbody className="analyst__tbody">
+                                {
+                                    Object.values(dateTable).map(val => {
+                                        return (
+                                            <tr className="analyst__tr" key={val.id_car}>
+                                                <td className="analyst__td">{val.license_plate}</td>
+                                                <td className="analyst__td">{val.region}</td>
+                                                <td className="analyst__td">{val.car_brand}</td>
+                                                <td className="analyst__td">{val.arrival_time}</td>
+                                                <td className="analyst__td">{val.departure_time}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        {console.log(dateTable)}
+                        {
+                            <ExcelFile filename={"Отчет " + pickedDate.toLocaleDateString()} element={<button type='button' className="button button--blue signin__button signin__button--center">Экспорт в Excel</button>}>
+                                <ExcelSheet data={dateTable} name={"Отчет " + pickedDate.toLocaleDateString()}>
+                                    <ExcelColumn label="Номер автомобиля" value="license_plate"/>
+                                    <ExcelColumn label="Регион" value="region"/>
+                                    <ExcelColumn label="Марка автомобиля" value="car_brand"/>
+                                    <ExcelColumn label="Время въезда" value="arrival_time"/>
+                                    <ExcelColumn label="Время выезда" value="departure_time"/>
+                                </ExcelSheet>
+                            </ExcelFile>
+                        }
+                    </div>
                     
                 }
                 
