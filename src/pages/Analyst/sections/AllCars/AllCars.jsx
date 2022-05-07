c75333ddd07ed2307eb89784d61a5db3047f98a4
DariaIS <dariastarodubova58@yandex.ru>
2022-05-07 17:59:51 +0300

    did the whole sorting for cars and its export files

--- src/pages/Analyst/sections/AllCars/AllCars.jsx
+++ src/pages/Analyst/sections/AllCars/AllCars.jsx
@@ -1,30 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 
 import { Header } from '../../../../components';
 import { AnalystNavigation } from '../../AnalystNavigation';
 
 import { ProtectedRoute } from '../../../../components/ProtectedRoute';
-import { Table } from '../../../../components/Table';
-
-import { useAllCars } from './hooks/useAllCars';
+import { CarsTable } from './components/CarsTable';
 
 export const AllCars = () => {
-    const {
-        carTable,
-        setCarTable,
-        exportPDF,
-        ExcelFile,
-        ExcelSheet,
-        ExcelColumn
-    } = useAllCars();
+
+    const [cars, setCars] = useState('');
 
     useEffect(() => {
-        // console.log('car')
+        console.log('car')
         Axios.get('http://localhost:3001/carTable').then((response) => {
-            setCarTable(response.data.result);
+            setCars(response.data.result);
         });
-    }, [setCarTable]);
+    }, [setCars]);
 
     return (
         <ProtectedRoute role='analyst' setNewRole={null}>
@@ -34,22 +26,10 @@ export const AllCars = () => {
 
             <div className="allCars section container">
                 {
-                    carTable &&
+                    cars &&
                     <div className="table">
                         <span className="table__title title title--medium">Все машины, присутствующие в базе данных</span>
-                        <div className="allCars__export-buttons">
-                            {<ExcelFile filename={"Все автомобили на " + new Date().toLocaleDateString()} element={<button type='button' className="button button--white signin__button">Экспорт Excel</button>}>
-                                <ExcelSheet data={carTable} name={"Все автомобили на " + new Date().toLocaleDateString()}>
-                                    <ExcelColumn label="Номер автомобиля" value="license_plate" />
-                                    <ExcelColumn label="Марка автомобиля" value="car_brand" />
-                                    <ExcelColumn label="ФИО владельца" value="name" />
-                                    <ExcelColumn label="Дата предоставления доступа" value="start_date" />
-                                    <ExcelColumn label="Дата истечения прав доступа" value="expiration_date" />
-                                </ExcelSheet>
-                            </ExcelFile>}
-                            {<button className="button button--blue signin__button" onClick={exportPDF}>Экспорт PDF</button>}
-                        </div>
-                        <Table
+                        <CarsTable
                             headers={
                                 [['Номер машины', 'license_plate'],
                                 ['Марка машины', 'car_brand'],
@@ -57,34 +37,8 @@ export const AllCars = () => {
                                 ['Дата предоставления доступа', 'start_date'],
                                 ['Дата истечения прав доступа', 'expiration_date']]
                             }
-                            data={carTable}
-                            setData={setCarTable} />
-                        {/* <table className="table__item">
-                        <thead className="table__thead">
-                            <tr className="table__tr">
-                                <th className="table__th">Номер машины</th>
-                                <th className="table__th">Марка машины</th>
-                                <th className="table__th">ФИО владельца</th>
-                                <th className="table__th">Дата предоставления доступа</th>
-                                <th className="table__th">Дата истечения прав доступа</th>
-                            </tr>
-                        </thead>
-                        <tbody className="table__tbody">
-                            {
-                                Object.values(carTable).map(elem => {
-                                    return (
-                                        <tr className="table__tr" key={elem.id_car}>
-                                            <td className="table__td">{elem.license_plate}</td>
-                                            <td className="table__td">{elem.car_brand}</td>
-                                            <td className="table__td">{elem.name}</td>
-                                            <td className="table__td">{elem.start_date}</td>
-                                            <td className="table__td">{elem.expiration_date}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table> */}
+                            data={cars}
+                            setData={setCars} />
                     </div>
                 }
             </div>
--- src/pages/Analyst/sections/AllCars/components/CarsTable/hooks/useCarsTable.jsx
+++ src/pages/Analyst/sections/AllCars/components/CarsTable/hooks/useCarsTable.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 
 import ReactExport from "react-export-excel";
 
-export const useAllCars = () => {
-    const [carTable, setCarTable] = useState('');
+export const useCarsTable = (items) => {
+    const [sortConfig, setSortConfig] = useState(null);
 
     const ExcelFile = ReactExport.ExcelFile;
     const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
     const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
-    const exportPDF = () => {
+    const exportPDF = (table) => {
         const unit = "pt";
         const size = "A4"; // Use A1, A2, A3 or A4
         const orientation = "portrait"; // portrait or landscape
@@ -21,7 +21,8 @@ export const useAllCars = () => {
 
         const headers = [['Номер автомобиля', 'Марка автомобиля', 'ФИО владельца', 'Дата предоставления доступа', 'Дата истечения прав доступа']];
 
-        const data = carTable.map(elem => [
+        console.log(table)
+        const data = table.map(elem => [
             elem.license_plate,
             elem.car_brand,
             elem.name,
@@ -49,14 +50,38 @@ export const useAllCars = () => {
             }
         };
 
-
         doc.autoTable(content)
         doc.save('Все автомобили на ' + new Date().toLocaleDateString() + '.pdf');
     }
 
+    const sortedItems = useMemo(() => {
+        let sortableItems = [...items];
+        if (sortConfig !== null) {
+            sortableItems.sort((a, b) => {
+                if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortableItems;
+    }, [items, sortConfig]);
+
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    }
+
     return {
-        carTable,
-        setCarTable,
+        items: sortedItems,
+        requestSort,
+        sortConfig,
         exportPDF,
         ExcelFile,
         ExcelSheet,
