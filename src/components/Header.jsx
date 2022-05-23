8d4b80d4d866e9b7a93a6cc4852a1a1e3adf6fde
DariaIS <dariastarodubova58@yandex.ru>
2022-05-23 13:59:23 +0300

    fix adaprive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Axios from 'axios';
 
+import { ReactComponent as CloseMenu } from '../assets/img/icons/closeMenu.svg';
+import { ReactComponent as OpenMenu } from '../assets/img/icons/openMenu.svg';
+
+
 export const Header = ({ children }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
 
     const logOut = () => {
         Axios.get('http://localhost:3001/logout');
@@ -10,6 +17,7 @@ export const Header = ({ children }) => {
 
     return (
         <header className="header">
+            <div className={`overflow ${isOpen && `overflow--visible`}`} onClick={() => setIsOpen(false)}></div>
             <div className="header__content container">
                 <h1 className="header__logo">
                     <Link className="header__logo-link" to='/home'>
@@ -18,8 +26,12 @@ export const Header = ({ children }) => {
                     </Link>
                 </h1>
                 <div className="header__nav">
-                    <nav className="nav">
+                    <OpenMenu className="header__open-nav" onClick={() => setIsOpen(true)} />
+                    <nav className={`nav ${isOpen && `nav--active`}`}>
                         <ul className="nav__list">
+                            <li className="nav__item nav__item--close">
+                                <CloseMenu className="nav__close-icon" onClick={() => setIsOpen(false)} />
+                            </li>
                             {children}
                             <li className="nav__item">
                                 <Link to='/' className="nav__link nav__link--blue" onClick={logOut}>Выйти</Link>
